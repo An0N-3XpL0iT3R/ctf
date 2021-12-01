de43925620d08dd1115e6790284450c0eda40a4f
An0N3XpL0iT3R <71928263+An0N-3XpL0iT3R@users.noreply.github.com>
2021-12-01 11:37:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-       function value(hi) { 
-			return hi.split("").reverse().join("")
+function value(hi) { 
+	return hi.split("").reverse().join("")
+	}
+function check(val){
+	if (val == "RickRolls") {				
+		alert("/" + atob(value("=42bpR3YuVnZ")) +".html "+ "flag is not here")
 		}
-		function check(val){
-			if (val == "RickRolls") {				
-				alert("/" + atob(value("=42bpR3YuVnZ")) +".html "+ "flag is not here")
-			}
-		}
+    }
