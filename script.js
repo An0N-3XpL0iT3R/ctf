3ab1d0c179e74871fbe979dbf4539fca2fd24c2d
An0N3XpL0iT3R <71928263+An0N-3XpL0iT3R@users.noreply.github.com>
2021-12-01 11:33:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+ <script type="">
+        function value(hi) { 
+			return hi.split("").reverse().join("")
+		}
+		function check(val){
+			if (val == "RickRolls") {				
+				alert("/" + atob(value("=42bpR3YuVnZ")) +".html "+ "flag is not here")
+			}
+		}
+    </script>
